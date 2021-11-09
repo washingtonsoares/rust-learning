03d116680361df79b6f5d9f96be025ba9cab07e6
Washington Soares <washingtonudi@gmail.com>
2021-11-09 20:27:50 -0300

    feat(init): ༼ つ ◕_◕ ༽つ add hello world

---
+++ hello_world/target/debug/deps/hello_world-b288ddbe3012959e.d
@@ -0,0 +1,5 @@
+/Users/washington/projects/rust-learning/hello_world/target/debug/deps/hello_world-b288ddbe3012959e: src/main.rs
+
+/Users/washington/projects/rust-learning/hello_world/target/debug/deps/hello_world-b288ddbe3012959e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/washington/projects/rust-learning/hello_world/target/debug/hello_world: /Users/washington/projects/rust-learning/hello_world/src/main.rs
