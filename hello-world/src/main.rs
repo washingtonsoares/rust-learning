aee2781e74d7dc8adf54d08a06abb71198e58c28
Washington Soares <washingtonudi@gmail.com>
2024-08-21 18:18:11 -0300

    Add functions

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let msg: &str = get_hello_message();
+    println!("{}", msg);
+}
+
+fn get_hello_message() -> &'static str {
+    "hello message"
 }
